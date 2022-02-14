87d7e6032460165ae66065b324b796f2e2435c28
YJL626 <909861220@QQ.COM>
2022-02-14 13:54:41 +0800

    cesium contianer update

--- src/views/CesiumContainer.vue
+++ src/views/CesiumContainer.vue
@@ -4,7 +4,7 @@
 <script setup lang="ts">
 import * as Cesium from 'cesium'
 import 'cesium/Build/Cesium/Widgets/widgets.css'
-import { onMounted } from 'vue'
+import { onMounted, watchEffect } from 'vue'
 
 import { envs } from '@/env/envs'
 import { useParkingStore } from '@/stores/parkingStore'
@@ -21,15 +21,24 @@ const initCesium = () => {
   })
 
   // Add Cesium OSM Buildings, a global 3D buildings layer.
-  const buildingTileset = viewer.scene.primitives.add(Cesium.createOsmBuildings())
+  //  const buildingTileset = viewer.scene.primitives.add(Cesium.createOsmBuildings())
   // Fly the camera to San Francisco at the given longitude, latitude, and height.
   viewer.camera.flyTo({
-    destination: Cesium.Cartesian3.fromDegrees(+envs.LONGITUDE, +envs.LATITUDE, 400),
+    destination: Cesium.Cartesian3.fromDegrees(112.17907, 34.51523, 200),
     orientation: {
       heading: Cesium.Math.toRadians(0.0),
       pitch: Cesium.Math.toRadians(-15.0)
     }
   })
+  watchEffect(() =>
+    parkingStore.remoteList.forEach(item => {
+      viewer.entities.add({
+        description: `Location: (${item.longitude}, ${item.latitude}, 10)`,
+        position: Cesium.Cartesian3.fromDegrees(+item.longitude, +item.latitude, 200),
+        point: { pixelSize: 100, color: Cesium.Color.RED }
+      })
+    })
+  )
 }
 onMounted(initCesium)
 </script>
